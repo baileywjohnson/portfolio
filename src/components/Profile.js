9c56b901601d7672a3c20d609d7e122ad9964cf1
Bailey Johnson <baileywjohnson@gmail.com>
2021-02-12 16:03:40 -0600

    More Styling and Content

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,11 +1,15 @@
 import './Profile.css'
 
+import {useState} from 'react'
+
 function Profile() {
+    const [gifShown, setGifShown] = useState(false);
+
     return (
         <div className="Profile">
-            <img id="headshot" alt="Photograph" src={process.env.PUBLIC_URL + '/headshot.png'} />
+            <img id="headshot" onMouseEnter={() => setGifShown(true)} onMouseLeave={() => setGifShown(false)} alt="Photograph" src={gifShown ? process.env.PUBLIC_URL + '/headshot.gif' : process.env.PUBLIC_URL + '/headshot.png'} />
             <div id="profileText">
-                <h1 id="profileName">Bailey Johnson</h1>
+                <h2 id="profileName">Bailey Johnson</h2>
                 <h4 id="profileTitle">Full-Stack Software Engineer</h4>
             </div>
         </div>
